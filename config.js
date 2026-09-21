bdfc3d847fde46b44edde7e48ecceb62fde22e01
Ifsha530 <ifshaqctblp@gmail.com>
2026-09-21 16:30:57 +0500

    Update Firebase configuration in config.js
    Updated Firebase API key and added measurement ID.

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 // Firebase Web App configuration.
 window.NATUREGEN_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyCvfD8V60yugkos26GoVIvI5Pga6BxQ2mg",
+  apiKey: "AIzaSyCvfD8V6Oyugkos26GoVIvI5Pga6BxQ2mg",
   authDomain: "naturegen-distribution.firebaseapp.com",
   projectId: "naturegen-distribution",
   storageBucket: "naturegen-distribution.firebasestorage.app",
   messagingSenderId: "321429469336",
-  appId: "1:321429469336:web:714ca31682b5db280b38ed"
-};
+  appId: "1:321429469336:web:714ca31682b5db280b38ed",
+  measurementId: "G-DQQ5JEFWZM"
